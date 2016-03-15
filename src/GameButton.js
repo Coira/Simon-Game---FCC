18f45dc2e48997b364ac9a7146d8fc65aff5e1e1
Coira <coira.ells@gmail.com>
2016-03-15 23:33:45 +0000

    create random sequence of button presses for the user to memorise, animation to light up the buttons in order

--- src/GameButton.js
+++ src/GameButton.js
@@ -6,10 +6,31 @@ import './style.scss';
 class GameButton extends React.Component {
     constructor(props) {
 	super(props);
+	this.state = {lit: false};
+	this.anim = null;
+	this.animStart = null;
     }
+
+    highlight() {
+	this.setState({lit:true});
+    }
+
+    unhighlight() {
+	this.setState({lit:false});
+    }
+
+	
     render() {
-	return (
-	    <div className={classnames("gameBtn", this.props.position)}></div>
+	let cn = this.props.position;
+	let cnLit = cn+"Highlight";
+
+	return (<div className=
+		     {classnames("gameBtn",
+				 cn,
+				 {[cnLit]: this.state.lit},
+				 {"disableClicks":this.props.disableClicks})}
+		     onClick={this.props.clicked}>
+	    </div>
 	);
     }
     
--- src/index.js
+++ src/index.js
@@ -8,12 +8,101 @@ import GameButton from './GameButton';
 class App extends React.Component {
     constructor(props) {
 	super(props);
+
+	this.state = {
+	    disableClicks: true,
+	    switchedOn: true
+	};
+	this.sequenceCount = 0;
+	this.sequence = [];
+	this.anim = null;
+	this.pIndex = 0;
     }
 
-    startGame() {
-	console.log("started");
+    turnOnGame() {
+	// click handler for switch button
     }
     
+    startGame() {
+	// click handler for start button
+
+	if (this.state.switchedOn) {
+	    this.sequenceCount = 0;
+	    this.sequence = [];
+	    this.compTurn();
+	}
+    }
+
+    compTurn() {
+	console.log("comp turn");
+
+	this.createSequence();
+	this.displaySequence();
+	this.playerTurn();
+    }
+
+    playerTurn() {
+
+	this.pIndex = 0;
+	// now wait for player to enter sequence
+    }
+    
+    createSequence() {
+	this.sequenceCount++;
+	this.sequence.push(Math.floor(Math.random() * 4));
+	//this.sequence.push(0);
+    }
+
+    displaySequence() {
+	console.log(this.sequence);
+
+	if (this.anim === null && this.sequence.length > 0) {
+	    let animStart = 0;
+	    let index = 0;
+	    let ref = this.sequence[index];
+	    this.setState({disableClicks: true});
+
+	    this.anim = setInterval(function() {
+		animStart += 100;
+		if (animStart === 500) {
+		    this.refs[ref].highlight();
+		}
+		else if (animStart >= 1100) {
+		    this.refs[ref].unhighlight();
+		    index++;
+		    ref = this.sequence[index];
+		    if (index < this.sequence.length) {
+			animStart = 0;
+		    }
+		    else {
+			clearInterval(this.anim);
+			this.anim = null;
+			this.setState({disableClicks: false});
+		    }
+		}
+	    }.bind(this), 100);
+	}
+    }
+
+    
+    gameBtnClicked(ref) {
+	console.log(ref);
+	if (ref === this.sequence[this.pIndex]) {
+	    console.log("correct");
+	}
+	else {
+	    console.log("incorrect");
+	}
+
+	this.pIndex++;
+	if (this.pIndex === this.sequence.length) {
+	    this.compTurn();
+	}
+	
+    }
+    
+    
+
     render() {
 	return (
 	    <div>
@@ -27,7 +116,7 @@ class App extends React.Component {
 			    </div>
 			    
 			    <div className="vert start">
-				<div onClick={this.startGame}
+				<div onClick={this.startGame.bind(this)}
 				     className="icon"></div>
 				<div className="label">START</div>
 			    </div>
@@ -46,10 +135,22 @@ class App extends React.Component {
 			</div>
 		    </div>
 		    
-		    <GameButton position="topLeft"></GameButton>
-		    <GameButton position="topRight"></GameButton>
-		    <GameButton position="bottomLeft"></GameButton>
-		    <GameButton position="bottomRight"></GameButton>
+		    <GameButton position="topLeft"
+				disableClicks={this.state.disableClicks}
+				clicked={this.gameBtnClicked.bind(this, 0)}
+				ref="0"></GameButton>
+		    <GameButton position="topRight"
+				disableClicks={this.state.disableClicks}
+				clicked={this.gameBtnClicked.bind(this, 1)}
+				ref="1"></GameButton>
+		    <GameButton position="bottomLeft"
+				disableClicks={this.state.disableClicks}
+				clicked={this.gameBtnClicked.bind(this, 2)}
+				ref="2"></GameButton>
+		    <GameButton position="bottomRight"
+				disableClicks={this.state.disableClicks}
+				clicked={this.gameBtnClicked.bind(this, 3)}
+				ref="3"></GameButton>
 
 		</div>
 	    </div>
